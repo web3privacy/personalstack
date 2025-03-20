143375a9d664656d7e6b93be5cbe9118434fd158
Agonist <bastien.billey@gmail.com>
2025-03-20 13:50:05 +0100

    add base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/personalstack",
   images: {
     // TODO add appropriate setup
     unoptimized: true,
