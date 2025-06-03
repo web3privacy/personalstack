99f5849980c72a37bcbb0d80c9c6f8096962805a
Mykola Siusko <116563801+Msiusko@users.noreply.github.com>
2025-06-04 00:25:47 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <main className="flex flex-col gap-8 mb-16">
       <PageHeader
         img="/images/banner.jpg"
-        title="privacy stack explorer"
-        tagline="explore what privacy stack experts are using"
+        title="personal FOSS stack explorer"
+        tagline="explore what personal FOSS stack experts are using"
       />
       <h3 className="w-full text-center">featured experts</h3>
       <StacksGrid stacks={data.stacks} tools={data.tools} take={3} />
